cca00cc84f24917d42ea0b8c009fad4060b824b7
Youssef Berrada <yberrada@mit.edu>
2018-11-29 10:33:20 -0500

    add accuracy code

---
+++ src/code_competition.R
@@ -0,0 +1,27 @@
+###                                             ###
+### 15.459 Financial Data Science and Computing ###
+###             Youssef Berrada                 ###
+###             yberrada@mit.edu                ###
+###                Fall 2018                    ###
+
+library(data.table)
+library(tidyverse)
+
+output = fread('/Users/youssefberrada/Desktop/output-results.csv')
+colnames(output) = c('id','article','CCAT','ECAT','GCAT','MCAT')
+
+output = output %>%
+  select(CCAT,ECAT,GCAT,MCAT)%>%
+  sapply(as.numeric)
+
+pred = fread('/Users/youssefberrada/Downloads/firstname_lastname.csv')
+
+pred = pred %>%
+  select(CCAT,ECAT,MCAT,GCAT)%>%
+  sapply(as.numeric)
+
+# This is a pooled Micro-average Accuracy measure
+CM = as.matrix(table(Actual = output, Predicted = pred))
+accuracy = sum(diag(CM))/sum(CM)
+accuracy
+
